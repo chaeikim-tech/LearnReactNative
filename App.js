e3b83ea31fba3f6b9819de2cece04e731267decd
chaeikim <sb02348@hanmail.net>
2022-06-10 11:10:18 +0900

    make Counter

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
 import React, {useState} from 'react';
-import {SafeAreaView, Button} from 'react-native';
-import Box from './components/Box';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import Counter from './components/Counter';
 
 // SafeAreaView 컴포넌트는 iPhone X 이상 기종에서 디스플레이의 보이지 않는 영역 및 최하단 영역에 내용이 보여지는 것을 방지
 
 const App = () => {
-  const [visible, setVisible] = useState(true);
-  const onPress = () => {
-    setVisible(!visible);
-  };
+  const [count, setCount] = useState(0);
+
+  const onIncrease = () => setCount(count + 1);
+  const onDecrease = () => setCount(count - 1);
+
   return (
-    <SafeAreaView>
-      <Button title="토글" onPress={onPress} />
-      {visible ? <Box rounded={true} color="blue" size="large" /> : null}
+    <SafeAreaView style={styles.full}>
+      <Counter count={count} onIncrease={onIncrease} onDecrease={onDecrease} />
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  full: {
+    flex: 1,
+  },
+});
 
 export default App;
