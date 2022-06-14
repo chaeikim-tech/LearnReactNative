2e8ca13e5deb4b18774b3f9dd9c076ddd7b34c1d
chaeikim <sb02348@hanmail.net>
2022-06-14 15:06:18 +0900

    TodoList empty page

--- App.js
+++ App.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
 import DateHead from './components/DateHead';
+import AddTodo from './components/AddTodo';
+import Empty from './components/Empty';
 
 function App() {
   const today = new Date();
 
   return (
-    <SafeAreaView>
+    <SafeAreaView edges={['bottom']} style={styles.block}>
       <DateHead date={today} />
+      <Empty />
+      <AddTodo />
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+  },
+});
 
 export default App;
