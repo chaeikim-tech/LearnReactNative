49f67d6685c5a62cafe38dcfcfcc1a52b2446c24
chaeikim <sb02348@hanmail.net>
2022-06-14 14:19:21 +0900

    Change StatusBar

--- App.js
+++ App.js
@@ -1,26 +1,17 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
-import Counter from './components/Counter';
+import DateHead from './components/DateHead';
 
-// SafeAreaView 컴포넌트는 iPhone X 이상 기종에서 디스플레이의 보이지 않는 영역 및 최하단 영역에 내용이 보여지는 것을 방지
-
-const App = () => {
-  const [count, setCount] = useState(0);
-
-  const onIncrease = () => setCount(count + 1);
-  const onDecrease = () => setCount(count - 1);
+function App() {
+  const today = new Date();
 
   return (
-    <SafeAreaView style={styles.full}>
-      <Counter count={count} onIncrease={onIncrease} onDecrease={onDecrease} />
+    <SafeAreaView>
+      <DateHead date={today} />
     </SafeAreaView>
   );
-};
+}
 
-const styles = StyleSheet.create({
-  full: {
-    flex: 1,
-  },
-});
+const styles = StyleSheet.create({});
 
 export default App;
